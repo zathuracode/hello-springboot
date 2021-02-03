16fd9928ce81a4a6e50a95638d05a3e5a08861d4
Diego Armando Gomez <diegomezusb@gmail.com>
2021-02-03 08:50:19 -0500

    Se corrigue el bug en HelloController

--- src/test/java/co/com/coomeva/devops/hello/controller/HelloControllerTest.java
+++ src/test/java/co/com/coomeva/devops/hello/controller/HelloControllerTest.java
@@ -23,7 +23,7 @@ class HelloControllerTest {
 	void testIndex()throws Exception {
 		
 		//Arrange
-		String expect="Hello from Spring Boot and Azure Devops";
+		String expect="Hola desde Spring Boot Y Azure Devops";
 		
 		//Act
 		mockMvc.perform(MockMvcRequestBuilders.get("/")
@@ -37,7 +37,7 @@ class HelloControllerTest {
 		
 		//Arrange
 		String name="Juan";
-		String expect="Hello "+name+" from Spring Boot and Azure Devops";
+		String expect="Hola "+name+" desde Spring Boot Y Azure Devops";
 		
 		//Act
 		mockMvc.perform(MockMvcRequestBuilders.get("/"+name)
